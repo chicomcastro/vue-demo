a1e7cf1d99752c6302e7a82e9b393ffecbc8cc2f
chicomcastro <franciscommcastro@gmail.com>
2022-12-15 00:34:10 +0000

    [ci skip] gh-pages

---
+++ precache-manifest.78c7328724da1268736a99c1f9923914.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116dadc527436334a6cf",
+    "url": "/vue-learning/css/app.7c91b495.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-learning/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "89478f4151db18a50bf0a2e3805b9a34",
+    "url": "/vue-learning/index.html"
+  },
+  {
+    "revision": "f20b63bcd90eb5139a78",
+    "url": "/vue-learning/js/about.e09c0622.js"
+  },
+  {
+    "revision": "116dadc527436334a6cf",
+    "url": "/vue-learning/js/app.ae986ee9.js"
+  },
+  {
+    "revision": "a0dd2b8b5836cb17ec09",
+    "url": "/vue-learning/js/chunk-vendors.92dff902.js"
+  },
+  {
+    "revision": "872e748f8ea107b835baa1524ed028b0",
+    "url": "/vue-learning/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-learning/robots.txt"
+  }
+]);
